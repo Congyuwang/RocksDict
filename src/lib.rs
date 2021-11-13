88f10452984aba86ed31a3110dda58f6d1dcbebe
Congyuwang <congyuwang@link.cuhk.edu.cn>
2021-11-13 16:27:31 +0800

    add support for int, string, float, bytes values
    - add support for float keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 use num_cpus;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
+use pyo3::types::{PyBytes, PyFloat, PyInt, PyString};
 use rocksdb::{Options, PlainTableFactoryOptions, SliceTransform, WriteOptions, DB};
 use std::fs::{create_dir_all, remove_dir_all};
 use std::ops::Deref;
 use std::path::Path;
+use pyo3::{PyTypeInfo, PyTryFrom};
 
 #[pyclass]
 struct Rdict {
     db: DB,
-    len: usize,
     write_opt: WriteOptions,
 }
 
@@ -22,6 +22,9 @@ impl Deref for Rdict {
     }
 }
 
+///
+/// Note that we do not support __len__()
+///
 #[pymethods]
 impl Rdict {
     #[new]
@@ -33,7 +36,6 @@ impl Rdict {
             Ok(_) => match DB::open(&default_options(), &path) {
                 Ok(db) => Ok(Rdict {
                     db,
-                    len: 0,
                     write_opt,
                 }),
                 Err(e) => Err(PyException::new_err(e.to_string())),
@@ -42,28 +44,32 @@ impl Rdict {
         }
     }
 
-    fn __len__(&self) -> PyResult<usize> {
-        Ok(self.len)
-    }
-
-    fn __getitem__<'a>(&self, key: &PyBytes, py: Python<'a>) -> PyResult<&'a PyBytes> {
+    fn __getitem__(&self, key: &PyAny, py: Python) -> PyResult<PyObject> {
+        let key = convert_key(key, py)?;
         match self.get_pinned(key.as_bytes()) {
             Ok(value) => match value {
                 None => Err(PyException::new_err("key not found")),
-                Some(slice) => Ok(PyBytes::new(py, slice.as_ref())),
+                Some(slice) => decode_value(py, slice.as_ref()),
             },
             Err(e) => Err(PyException::new_err(e.to_string())),
         }
     }
 
-    fn __setitem__(&self, key: &PyBytes, value: &PyBytes) -> PyResult<()> {
-        match self.put_opt(key.as_bytes(), value.as_bytes(), &self.write_opt) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(PyException::new_err(e.to_string())),
+    fn __setitem__(&self, key: &PyAny, value: &PyAny, py: Python) -> PyResult<()> {
+        let key = convert_key(key, py)?;
+        match encode_value(value) {
+            Ok(value) => {
+                match self.put_opt(key.as_bytes(), value, &self.write_opt) {
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(PyException::new_err(e.to_string())),
+                }
+            }
+            Err(e) => Err(PyException::new_err(e.to_string()))
         }
     }
 
-    fn __contains__(&self, key: &PyBytes) -> PyResult<bool> {
+    fn __contains__(&self, key: &PyAny, py: Python) -> PyResult<bool> {
+        let key = convert_key(key, py)?;
         match self.get_pinned(key.as_bytes()) {
             Ok(value) => match value {
                 None => Ok(false),
@@ -73,8 +79,16 @@ impl Rdict {
         }
     }
 
-    fn __delitem__(&self, key: &PyBytes) -> PyResult<()> {
+    fn __delitem__(&self, key: &PyAny, py: Python) -> PyResult<()> {
+        let key = convert_key(key, py)?;
         match self.delete_opt(key.as_bytes(), &self.write_opt) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(PyException::new_err(e.to_string())),
+        }
+    }
+
+    fn close(&self) -> PyResult<()> {
+        match self.db.flush() {
             Ok(_) => Ok(()),
             Err(e) => Err(PyException::new_err(e.to_string())),
         }
@@ -92,6 +106,127 @@ impl Rdict {
 fn rocksdict(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Rdict>()?;
     Ok(())
+}
+
+enum ValueTypes<'a> {
+    Bytes(&'a[u8]),
+    String(String),
+    Int(i64),
+    Float(f64),
+    Unsupported,
+}
+
+fn encoding_byte(v_type: &ValueTypes) -> u8 {
+    match v_type {
+        ValueTypes::Bytes(_) => 1,
+        ValueTypes::String(_) => 2,
+        ValueTypes::Int(_) => 3,
+        ValueTypes::Float(_) => 4,
+        ValueTypes::Unsupported => 0,
+    }
+}
+
+///
+/// Convert string, int, float, bytes to byte encodings.
+///
+/// The first byte is used for encoding value types
+///
+fn encode_value(value: &PyAny) -> Result<Box<[u8]>, &str> {
+    let bytes = if PyBytes::is_type_of(value) {
+        let bytes: &PyBytes = PyTryFrom::try_from(value).unwrap();
+        ValueTypes::Bytes(bytes.as_bytes())
+    } else if PyString::is_type_of(value) {
+        let value: &PyString = PyTryFrom::try_from(value).unwrap();
+        ValueTypes::String(value.to_string())
+    } else if PyInt::is_type_of(value) {
+        let value: &PyInt = PyTryFrom::try_from(value).unwrap();
+        let value: i64 = value.extract().unwrap();
+        ValueTypes::Int(value)
+    } else if PyFloat::is_type_of(value) {
+        let value: &PyFloat = PyTryFrom::try_from(value).unwrap();
+        let value: f64 = value.extract().unwrap();
+        ValueTypes::Float(value)
+    } else {
+        ValueTypes::Unsupported
+    };
+    let type_encoding = encoding_byte(&bytes);
+    match bytes {
+        ValueTypes::Bytes(value) => {
+            Ok(concat_type_encoding(type_encoding, value))
+        }
+        ValueTypes::String(value) => {
+            Ok(concat_type_encoding(type_encoding, value.as_bytes()))
+        }
+        ValueTypes::Int(value) => {
+            Ok(concat_type_encoding(type_encoding, &value.to_be_bytes()[..]))
+        }
+        ValueTypes::Float(value) => {
+            Ok(concat_type_encoding(type_encoding, &value.to_be_bytes()[..]))
+        }
+        ValueTypes::Unsupported => {
+            Err("Only support `string`, `int`, `float`, and `bytes` as values")
+        }
+    }
+}
+
+fn decode_value(py: Python, bytes: &[u8]) -> PyResult<PyObject> {
+    match bytes.get(0) {
+        None => Err(PyException::new_err("Unknown value type")),
+        Some(byte) => {
+            match byte {
+                1 => Ok(PyBytes::new(py, &bytes[1..]).to_object(py)),
+                2 => {
+                    let string = match String::from_utf8(bytes[1..].to_vec()) {
+                        Ok(s) => s,
+                        Err(_) => {
+                            return Err(PyException::new_err("utf-8 decoding error"))
+                        }
+                    };
+                    Ok(string.into_py(py))
+                },
+                3 => {
+                    let int: i64 = i64::from_be_bytes(bytes[1..].try_into().unwrap());
+                    Ok(int.into_py(py))
+                },
+                4 => {
+                    let int: f64 = f64::from_be_bytes(bytes[1..].try_into().unwrap());
+                    Ok(int.into_py(py))
+                },
+                _ => Err(PyException::new_err("Unknown value type")),
+            }
+        }
+    }
+}
+
+fn concat_type_encoding(encoding: u8, payload: &[u8]) -> Box<[u8]> {
+    let mut output = Vec::with_capacity(payload.len() + 1);
+    output.push(encoding);
+    output.extend_from_slice(payload);
+    output.into_boxed_slice()
+}
+
+///
+/// Convert string, int, bytes as values and keys.
+///
+fn convert_key<'a>(key: &'a PyAny, py: Python<'a>) -> PyResult<&'a PyBytes> {
+    if PyString::is_type_of(key) {
+        let key: &PyString = PyTryFrom::try_from(key).unwrap();
+        let key: String = key.to_string();
+        Ok(PyBytes::new(py, key.as_bytes()))
+    } else if PyBytes::is_type_of(key) {
+        let bytes: &PyBytes = PyTryFrom::try_from(key).unwrap();
+        Ok(bytes)
+    } else if PyInt::is_type_of(key) {
+        let key: &PyInt = PyTryFrom::try_from(key).unwrap();
+        let key: i64 = key.extract().unwrap();
+        Ok(PyBytes::new(py, &key.to_be_bytes()[..]))
+    } else if PyFloat::is_type_of(key) {
+        let key: &PyFloat = PyTryFrom::try_from(key).unwrap();
+        let key: f64 = key.extract().unwrap();
+        Ok(PyBytes::new(py, &key.to_be_bytes()[..]))
+    } else {
+        Err(PyException::new_err("Only support `string`, `int`, `float`, and `bytes` as keys"))
+    }
 }
 
 fn default_options() -> Options {
--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import unittest
 from rocksdict import Rdict
-from random import randint
+from random import randint, random, randbytes
 
 
 TEST_INT_RANGE_UPPER = 9999999
@@ -9,21 +9,28 @@ TEST_INT_RANGE_UPPER = 9999999
 def compare_int_dicts(ref_dict: dict, test_dict: Rdict, lower: int, upper: int):
     # assert that the keys are the same
     for i in range(lower, upper):
-        assert (i in ref_dict) == (i.to_bytes(4, "little") in test_dict)
+        assert (i in ref_dict) == (i in test_dict)
 
     # assert that the values are the same
     for k, v in ref_dict.items():
-        assert k.to_bytes(4, "little") in test_dict
-        assert int.from_bytes(test_dict[k.to_bytes(4, "little")], "little")
+        assert k in test_dict
+        assert test_dict[k] == v
 
 
-class TestIntKeys(unittest.TestCase):
+def compare_dicts(ref_dict: dict, test_dict: Rdict):
+    # assert that the values are the same
+    for k, v in ref_dict.items():
+        assert k in test_dict
+        assert test_dict[k] == v
+
+
+class TestInt(unittest.TestCase):
     test_dict = None
     ref_dict = None
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.test_dict = Rdict("./temp")
+        cls.test_dict = Rdict("./temp_int")
         cls.ref_dict = dict()
 
     def test_add_integer(self):
@@ -31,7 +38,7 @@ class TestIntKeys(unittest.TestCase):
             key = randint(0, TEST_INT_RANGE_UPPER)
             value = randint(0, TEST_INT_RANGE_UPPER)
             self.ref_dict[key] = value
-            self.test_dict[key.to_bytes(4, "little")] = value.to_bytes(4, "little")
+            self.test_dict[key] = value
 
         compare_int_dicts(self.ref_dict, self.test_dict, 0, TEST_INT_RANGE_UPPER)
 
@@ -40,7 +47,7 @@ class TestIntKeys(unittest.TestCase):
             key = randint(0, TEST_INT_RANGE_UPPER)
             if key in self.ref_dict:
                 del self.ref_dict[key]
-                del self.test_dict[key.to_bytes(4, "little")]
+                del self.test_dict[key]
 
         compare_int_dicts(self.ref_dict, self.test_dict, 0, TEST_INT_RANGE_UPPER)
 
@@ -49,25 +56,89 @@ class TestIntKeys(unittest.TestCase):
         cls.test_dict.destroy()
 
 
-class TestStringKeys(unittest.TestCase):
+class TestFloat(unittest.TestCase):
+    test_dict = None
+    ref_dict = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_dict = Rdict("./temp_float")
+        cls.ref_dict = dict()
+
+    def test_add_float(self):
+        for i in range(10000):
+            key = random()
+            value = random()
+            self.ref_dict[key] = value
+            self.test_dict[key] = value
+
+        compare_dicts(self.ref_dict, self.test_dict)
+
+    def test_delete_float(self):
+        for i in range(5000):
+            keys = [k for k in self.ref_dict.keys()]
+            key = keys[randint(0, len(self.ref_dict) - 1)]
+            del self.ref_dict[key]
+            del self.test_dict[key]
+
+        compare_dicts(self.ref_dict, self.test_dict)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_dict.destroy()
+
+
+class TestBytes(unittest.TestCase):
+    test_dict = None
+    ref_dict = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_dict = Rdict("./temp_bytes")
+        cls.ref_dict = dict()
+
+    def test_add_bytes(self):
+        for i in range(10000):
+            key = randbytes(10)
+            value = randbytes(20)
+            self.ref_dict[key] = value
+            self.test_dict[key] = value
+
+        compare_dicts(self.ref_dict, self.test_dict)
+
+    def test_delete_bytes(self):
+        for i in range(5000):
+            keys = [k for k in self.ref_dict.keys()]
+            key = keys[randint(0, len(self.ref_dict) - 1)]
+            del self.ref_dict[key]
+            del self.test_dict[key]
+
+        compare_dicts(self.ref_dict, self.test_dict)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_dict.destroy()
+
+
+class TestString(unittest.TestCase):
     test_dict = None
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.test_dict = Rdict("./temp2")
+        cls.test_dict = Rdict("./temp_string")
 
     def test_string(self):
-        self.test_dict[b"Guangdong"] = b"Shenzhen"
-        self.test_dict[b"Sichuan"] = b"Changsha"
+        self.test_dict["Guangdong"] = "Shenzhen"
+        self.test_dict["Sichuan"] = "Changsha"
         # overwrite
-        self.test_dict[b"Sichuan"] = b"Chengdu"
-        self.test_dict[b"Beijing"] = b"Beijing"
-        del self.test_dict[b"Beijing"]
+        self.test_dict["Sichuan"] = "Chengdu"
+        self.test_dict["Beijing"] = "Beijing"
+        del self.test_dict["Beijing"]
 
         # assertions
-        assert b"Beijing" not in self.test_dict
-        assert self.test_dict[b"Sichuan"] == b"Chengdu"
-        assert self.test_dict[b"Guangdong"] == b"Shenzhen"
+        assert "Beijing" not in self.test_dict
+        assert self.test_dict["Sichuan"] == "Chengdu"
+        assert self.test_dict["Guangdong"] == "Shenzhen"
 
     @classmethod
     def tearDownClass(cls):
