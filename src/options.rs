c919647db502f403317d00b651e05708c7ac4813
Congyuwang <congyuwang@link.cuhk.edu.cn>
2021-12-29 20:18:38 +0800

    auto-formatting

--- src/options.rs
+++ src/options.rs
@@ -166,9 +166,11 @@ impl OptionsPy {
             SliceTransformType::Fixed(len) => SliceTransform::create_fixed_prefix(len),
             SliceTransformType::MaxLen(len) => match create_max_len_transform(len) {
                 Ok(f) => f,
-                Err(_) => return Err(PyException::new_err(
-                    "max len prefix only supports len from 1 to 128",
-                ))
+                Err(_) => {
+                    return Err(PyException::new_err(
+                        "max len prefix only supports len from 1 to 128",
+                    ))
+                }
             },
             SliceTransformType::NOOP => SliceTransform::create_noop(),
         };
@@ -733,7 +735,11 @@ impl PlainTableFactoryOptionsPy {
     pub(crate) fn to_rocks(&self) -> PlainTableFactoryOptions {
         PlainTableFactoryOptions {
             // One extra byte for python object type
-            user_key_length: if self.user_key_length > 0 { self.user_key_length + 1 } else { 0 },
+            user_key_length: if self.user_key_length > 0 {
+                self.user_key_length + 1
+            } else {
+                0
+            },
             bloom_bits_per_key: self.bloom_bits_per_key,
             hash_table_ratio: self.hash_table_ratio,
             index_sparseness: self.index_sparseness,
@@ -854,16 +860,11 @@ macro_rules! implement_max_len_transform {
     };
 }
 
-implement_max_len_transform!( 1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-                             11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                             21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-                             31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                             41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-                             51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-                             61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-                             71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                             81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-                             91, 92, 93, 94, 95, 96, 97, 98, 99,100,
-                            101,102,103,104,105,106,107,108,109,110,
-                            111,112,113,114,115,116,117,118,119,120,
-                            121,122,123,124,125,126,127,128);
+implement_max_len_transform!(
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+    75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+    99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117,
+    118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128
+);
