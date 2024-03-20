d740eb438ded591e6fb11e9bc3eccf817d991bb6
Congyu WANG <leonwang998@gmail.com>
2024-03-20 12:21:30 +0800

    remove outdated benchmarks

--- benchmark/conftest.py
+++
@@ -1,7 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--dbname", action="store", default="rocks_db")
-    parser.addoption("--num", action="store", default=1000)
-    parser.addoption("--k_size", action="store", default=16)
-    parser.addoption("--v_size", action="store", default=100)
-    parser.addoption("--batch_size", action="store", default=100)
-    parser.addoption("--percent", action="store", default=0.01)
